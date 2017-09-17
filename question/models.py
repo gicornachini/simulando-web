d7bad06ebc0dc985b61ac5d1eaf5da69cc833984
Giovanni Cornachini <giovannicornachini@gmail.com>
2017-09-17 14:47:04 -0300

    Criando API

---
+++ question/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Question(models.Model):
+    text = models.TextField()
+    correct_choice = models.ForeignKey('QuestionChoice', related_name='correct_choice', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class QuestionChoice(models.Model):
+    question = models.ForeignKey(Question)
+    text = models.TextField()
+    letter = models.CharField(max_length=1)
+
+    class Meta:
+        unique_together = ('question', 'letter')
