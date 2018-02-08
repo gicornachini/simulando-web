9868703d16465510a9f4c22ed2148312c0cd39ca
Bruno Soares <vsoares.bruno@gmail.com>
2018-02-08 18:09:04 -0200

    service question

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "../css/App.css";
 
+const API = 'http://www.json-generator.com/endpoint/json/get/cuInEjYfsi?indent=2';
+const DEFAULT_QUERY = 'redux';
+
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API + DEFAULT_QUERY)
+      .then(response => response.json())
+      .then(data => this.setState({ questions: data.questions }));
+  }
   render() {
+    const { questions } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Wrap-questions">
+          {questions.map(question =>
+            <div key={question.objectID}>
+              <a href={question.url}>{question.title}</a>
+            </div>
+          )}
+        </div>
       </div>
+
+      // <div className="App">
+      //   <header className="App-header">
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
     );
   }
 }
