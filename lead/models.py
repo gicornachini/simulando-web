901af16e5137a81877a9789d1c24f489e12f3ac0
Giovanni Cornachini <giovannicornachini@gmail.com>
2018-02-17 10:56:56 -0200

    python 3 and django 2.0 update

--- lead/models.py
+++ lead/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class UserLead(models.Model):
     """ Model to warehouse all leads. """
     email = models.EmailField()
--- question/models.py
+++ question/models.py
@@ -3,17 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Question(models.Model):
     text = models.TextField()
     correct_choice = models.ForeignKey('QuestionAlternative',
                                        related_name='correct_choice',
-                                       null=True, blank=True)
+                                       null=True, blank=True,
+                                       on_delete=models.CASCADE,)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class QuestionAlternative(models.Model):
-    question = models.ForeignKey(Question, related_name='question_choices')
+    question = models.ForeignKey(Question,
+                                 related_name='question_choices',
+                                 on_delete=models.CASCADE)
     text = models.TextField()
     letter = models.CharField(max_length=1)
 
